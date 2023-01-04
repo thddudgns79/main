b2218189cd6bbcc1dbbbc8134b3eb1e3652049e3
OEUNN <jyejyp5@gmail.com>
2023-01-04 10:06:46 +0900

    로그인, 로그아웃 기능

--- src/main/java/com/coderby/myapp/classes/controller/ClassController.java
+++ src/main/java/com/coderby/myapp/classes/controller/ClassController.java
@@ -1,14 +1,11 @@
 package com.coderby.myapp.classes.controller;
 
-import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.coderby.myapp.classes.model.ClassVO;
 import com.coderby.myapp.classes.service.IClassService;
