756be83d580036553da73cfd9c56681d332b3c7f
thddudrb79 <thddudrb79@naver.com>
2023-01-06 18:30:17 +0900

    지운것들

--- src/main/java/com/coderby/myapp/classes/service/IStudentService.java
+++
@@ -1,8 +0,0 @@
-package com.coderby.myapp.classes.service;
-
-import com.coderby.myapp.classes.model.StudentVO;
-
-public interface IStudentService {
-	StudentVO selectStudent(String stdId);
-	String getPassword(String stdId);
-}
